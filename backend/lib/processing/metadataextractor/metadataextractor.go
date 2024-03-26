c6a41c3c209535d638192c0976cb39029e4ec33e
Bongofriend <memiaslan1905@gmail.com>
2024-03-26 20:35:08 +0100

    init metadata extraction

--- backend/lib/processing/metadataextractor/metadataextractor.go
+++ backend/lib/processing/metadataextractor/metadataextractor.go
@@ -2,25 +2,86 @@ package metadataextractor
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"log"
+	"os"
 	"os/exec"
 	"sync"
 )
 
 type MetadataExtractor struct {
+	MetadataChan chan AudiobookMetadata
+}
+
+type AudiobookMetadata struct {
+	Chapters []struct {
+		ID        int    `json:"id"`
+		TimeBase  string `json:"time_base"`
+		Start     int    `json:"start"`
+		StartTime string `json:"start_time"`
+		End       int    `json:"end"`
+		EndTime   string `json:"end_time"`
+		Tags      struct {
+			Title string `json:"title"`
+		} `json:"tags"`
+	} `json:"chapters"`
+	Format struct {
+		Filename       string `json:"filename"`
+		NbStreams      int    `json:"nb_streams"`
+		NbPrograms     int    `json:"nb_programs"`
+		FormatName     string `json:"format_name"`
+		FormatLongName string `json:"format_long_name"`
+		StartTime      string `json:"start_time"`
+		Duration       string `json:"duration"`
+		Size           string `json:"size"`
+		BitRate        string `json:"bit_rate"`
+		ProbeScore     int    `json:"probe_score"`
+		Tags           struct {
+			MajorBrand       string `json:"major_brand"`
+			MinorVersion     string `json:"minor_version"`
+			CompatibleBrands string `json:"compatible_brands"`
+			Title            string `json:"title"`
+			Artist           string `json:"artist"`
+			Composer         string `json:"composer"`
+			Album            string `json:"album"`
+			Encoder          string `json:"encoder"`
+			Comment          string `json:"comment"`
+			Genre            string `json:"genre"`
+			MediaType        string `json:"media_type"`
+		} `json:"tags"`
+	} `json:"format"`
 }
 
 func NewMetadataExtractor() (MetadataExtractor, error) {
-	if !ffmpegIsAvailable() {
+	if !ffprobeIsAvailable() {
 		return MetadataExtractor{}, errors.New("ffmpeg is not installed or found")
 	}
-	return MetadataExtractor{}, nil
+	return MetadataExtractor{
+		MetadataChan: make(chan AudiobookMetadata),
+	}, nil
 }
 
 // TODO
 func (m MetadataExtractor) extractMetadata(path string) {
-
+	if stat, err := os.Stat(path); err != nil || stat.IsDir() {
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	ffprobeArgs := []string{"-print_format", "json", "-show_format", "-show_chapters", path}
+	cmd := exec.Command("ffprobe", ffprobeArgs...)
+	output, err := cmd.Output()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ffprobeOutput := AudiobookMetadata{}
+	if err := json.Unmarshal(output, &ffprobeOutput); err != nil {
+		log.Println(err)
+		return
+	}
+	m.MetadataChan <- ffprobeOutput
 }
 
 func (m MetadataExtractor) Process(ctx context.Context, wg *sync.WaitGroup, pathChan <-chan string) {
@@ -39,7 +100,7 @@ func (m MetadataExtractor) Process(ctx context.Context, wg *sync.WaitGroup, path
 	}()
 }
 
-func ffmpegIsAvailable() bool {
+func ffprobeIsAvailable() bool {
 	_, err := exec.LookPath("ffmpeg")
 	return err == nil
 }
--- backend/lib/processing/metadataextractor/metadataextractor_test.go
+++ backend/lib/processing/metadataextractor/metadataextractor_test.go
@@ -1,7 +1,11 @@
 package metadataextractor_test
 
 import (
+	"context"
+	"log"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/bongofriend/bookplayer/backend/lib/processing/metadataextractor"
 )
@@ -10,4 +14,39 @@ func TestNewMetadataExtractor(t *testing.T) {
 	if _, err := metadataextractor.NewMetadataExtractor(); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestMetaDataExtractorProcess(t *testing.T) {
+	extractor, _ := metadataextractor.NewMetadataExtractor()
+	pathChan := make(chan string)
+	context, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	wg := sync.WaitGroup{}
+	done := make(chan bool)
+
+	testfilePath := "/home/memi/projects/bookplayer/data/test.m4b"
+
+	go func() {
+		select {
+		case <-context.Done():
+			done <- false
+			close(done)
+			return
+		case data := <-extractor.MetadataChan:
+			log.Printf("%+v", data)
+			done <- true
+			close(done)
+		}
+	}()
+
+	extractor.Process(context, &wg, pathChan)
+	pathChan <- testfilePath
+	dataReceived := <-done
+
+	cancel()
+	wg.Wait()
+
+	if !dataReceived {
+		log.Fatal("No data received from MetadataExtractor")
+	}
+
 }
